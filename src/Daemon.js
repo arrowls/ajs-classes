4d35b88d3e58ecd66d046bd108275f5f567aefc0
arrowls <babuyoff@gmail.com>
2022-09-29 12:41:12 +0300

    task done

--- src/Daemon.js
+++ src/Daemon.js
@@ -1,6 +1,6 @@
-import Character from './Character';
+import Magician from './Magician';
 
-export default class Daemon extends Character {
+export default class Daemon extends Magician {
   constructor(name) {
     super(name, 'Daemon');
     this.attack = 10;
--- src/Magician.js
+++ src/Magician.js
@@ -1,9 +1,25 @@
 import Character from './Character';
 
 export default class Magician extends Character {
-  constructor(name) {
-    super(name, 'Magician');
+  constructor(name, type = 'Magician') {
+    super(name, type);
     this.attack = 10;
     this.defence = 40;
+    this.stoned = false;
+  }
+
+  set attack(distance) {
+    this.attack *= (10 - distance) / 10;
+    if (this.stoned) {
+      this.attack -= Math.log2(distance) * 5;
+    }
+  }
+
+  get attack() {
+    return this.attack;
+  }
+
+  set stoned(value = false) { // на случай, если "дурман" нужно будет отменить
+    this.stoned = value;
   }
 }
